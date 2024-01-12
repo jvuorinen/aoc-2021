b406fb3ebe99a6039c061e734046bddb2a3aef61
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-01-12 09:55:46 +0200

    cleaner

--- 2017/day_05.py
+++ 2017/day_05.py
@@ -8,10 +8,9 @@ def run(mem, part1=True):
     mem = mem.copy()
     c = i = 0
     while c < len(mem):
-        _c = mem[c]
         offset = 1 if part1 or mem[c] < 3 else -1
         mem[c] += offset
-        c += _c
+        c += mem[c] - offset
         i += 1
     return i
 
