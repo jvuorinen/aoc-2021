3a3291317c63512905d278158f836471a5eee89f
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-01-12 14:48:57 +0200

    cleaner

--- 2017/day_07.py
+++ 2017/day_07.py
@@ -4,38 +4,39 @@ import networkx as nx
 from utils import read, print_answers
 
 
-def parse():
+def parse_graph(raw):
     G = nx.DiGraph()
-    for line in read(2017, 7).split("\n"):
-        n, w = re.findall(r"([^ ]+) \((.+)\)", line)[0]
+    for line in raw:
+        n, w, *children = re.findall(r'\w+', line)
         G.add_node(n, weight=int(w))
-        if children := re.findall(r"-> (.*)", line):
-            for c in children[0].split(", "):
-                G.add_edge(n, c)
+        for c in children:
+            G.add_edge(n, c)
     return G
 
 
-def get_weight(G, node):
+def total_weight(G, node):
     desc_weight = sum([G.nodes[x]["weight"] for x in nx.descendants(G, node)])
     return G.nodes[node]["weight"] + desc_weight
 
 
 def get_problem(node):
     children = [x for x in G.successors(node)]
-    weights = [get_weight(G, x) for x in children]
+    weights = [total_weight(G, x) for x in children]
     counts = Counter(weights)
     if len(counts) > 1:
         bad_weight, good_weight = sorted(counts, key=counts.get)
         return children[weights.index(bad_weight)], good_weight
 
-G = parse()
+raw = read(2017, 7).split("\n")
+
+G = parse_graph(raw)
 root = next(nx.topological_sort(G))
 
 node = root
 while problem := get_problem(node):
     node, good_weight = problem
 
-adjustment = get_weight(G, node) - good_weight
+adjustment = total_weight(G, node) - good_weight
 a2 = G.nodes[node]["weight"] - adjustment
 
 print_answers(root, a2, day=7)  # azqje 646
