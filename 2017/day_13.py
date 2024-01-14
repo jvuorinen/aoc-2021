692075a880d371e1960738f05cd3fed01b94cee8
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-01-14 14:16:07 +0200

    cleaner

--- 2017/day_13.py
+++ 2017/day_13.py
@@ -1,20 +1,14 @@
 from utils import read, print_answers
 
+
+def is_caught(time, range):
+    return time % (range * 2 - 2) == 0
+
+
 raw = read(2017, 13).split("\n")
 fwall = [tuple(map(int, line.split(": "))) for line in raw]
 
-# Part 1
-a1 = sum([d * r * (d % (r * 2 - 2) == 0) for d, r in fwall])
+a1 = sum([d * r * is_caught(d, r) for d, r in fwall])
+a2 = next(dl for dl in range(5_000_000) if not any(is_caught(d + dl, r) for d, r in fwall))
 
-
-# Part 2
-def get_delay(fwall):
-    for delay in range(10_000_000):
-        for d, r in fwall:
-            if (d + delay) % (r * 2 - 2) == 0:
-                break
-        else:
-            return delay
-
-
-print_answers(a1, get_delay(fwall), day=13)  # 2688 3876272
+print_answers(a1, a2, day=13)  # 2688 3876272
