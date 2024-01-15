091593895003da25401e2840b93cf551fe9a944a
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-01-15 17:05:06 +0200

    d14

---
+++ 2017/day_14.py
@@ -0,0 +1,37 @@
+from functools import reduce
+import networkx as nx
+from utils import read, print_answers
+
+
+def knot(word, times=64):
+    lst = [x for x in range(256)]
+    inputs = [*map(ord, word)] + [17, 31, 73, 47, 23]
+    s = 0
+    for sk, ip in enumerate(inputs * times):
+        lst = lst[:ip][::-1] + lst[ip:]
+        p = (ip + sk) % len(lst)
+        s += p
+        lst = lst[p:] + lst[:p]
+    st = -s % len(lst)
+    lst = lst[st:] + lst[:st]
+    dense = [reduce(int.__xor__, lst[i : i + 16]) for i in range(0, len(lst), 16)]
+    hx = "".join([hex(x)[2:].zfill(2) for x in dense])
+    bn = "".join([bin(int(x, 16))[2:].zfill(4) for x in hx])
+    return bn
+
+
+word = read(2017, 14)
+
+ones = set([i + j * 1j for j in range(128) for i, c in enumerate(knot(f"{word}-{j}")) if c == "1"])
+
+G = nx.Graph()
+for x in ones:
+    G.add_edge(x, x)
+    for d in (1, 1j):
+        if (n := x + d) in ones:
+            G.add_edge(x, n)
+
+a1 = len(ones)
+a2 = nx.number_connected_components(G)
+
+print_answers(a1, a2, day=99)
