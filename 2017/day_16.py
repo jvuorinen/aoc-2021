70c1953b8e6da6e4ce92f50d35cb371f934eacbe
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-01-16 09:30:24 +0200

    d16

--- 2017/day_16.py
+++ 2017/day_16.py
@@ -1,16 +1,35 @@
-from itertools import combinations, permutations
-from functools import reduce
-from collections import Counter, defaultdict
-from math import prod
-import numpy as np
-import re
-import networkx as nx
 from utils import read, print_answers
 
-raw = read().split("\n")
-# raw = read(2017, 16).split("\n")
+
+def dance(word, cmds):
+    prgs = list(word)
+    for cmd in cmds:
+        c, rest = cmd[0], cmd[1:]
+        if c == "s":
+            i = int(rest)
+            prgs[:] = prgs[-i:] + prgs[:-i]
+        if c == "x":
+            a, b = map(int, rest.split("/"))
+            prgs[a], prgs[b] = prgs[b], prgs[a]
+        if c == "p":
+            ia, ib = map(prgs.index, rest.split("/"))
+            prgs[ia], prgs[ib] = prgs[ib], prgs[ia]
+    return "".join(prgs)
 
 
-a1 = None
-a2 = None
-print_answers(a1, a2, day=16)
+def dance_much(word, times):
+    seen = []
+    while True:
+        if word in seen:
+            return seen[times % len(seen)]
+        seen.append(word)
+        word = dance(word, cmds)
+
+
+cmds = read(2017, 16).split(",")
+word = "abcdefghijklmnop"
+
+a1 = dance(word, cmds)
+a2 = dance_much(word, int(1e9))
+
+print_answers(a1, a2, day=16)  # nlciboghjmfdapek nlciboghmkedpfja
