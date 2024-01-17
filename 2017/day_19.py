06d8d570a8d282db1f4bacd40c193c28fa08f984
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-01-17 22:42:12 +0200

    d19

--- 2017/day_19.py
+++ 2017/day_19.py
@@ -1,16 +1,24 @@
-from itertools import combinations, permutations
-from functools import reduce
-from collections import Counter, defaultdict
-from math import prod
-import numpy as np
-import re
-import networkx as nx
 from utils import read, print_answers
 
-raw = read().split("\n")
-# raw = read(2017, 19).split("\n")
+raw = read(2017, 19).split("\n")
+M = {j + i * 1j: c for i, row in enumerate(raw) for j, c in enumerate(row) if c != " "}
 
 
-a1 = None
-a2 = None
+def get_next(loc, hdg):
+    for _hdg in (hdg, hdg * 1j, hdg * -1j):
+        if (_loc := loc + _hdg) in M:
+            return _loc, _hdg
+
+
+loc = raw[0].index("|")
+hdg = 1j
+
+a1 = 1
+a2 = ""
+while nxt := get_next(loc, hdg):
+    a1 += 1
+    loc, hdg = nxt
+    if (c := M[loc]).isalpha():
+        a2 += c
+
 print_answers(a1, a2, day=19)
