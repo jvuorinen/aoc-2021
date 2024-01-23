883158b602b710398d1d9495b49f31124ac47701
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-01-23 15:53:15 +0200

    day 4

---
+++ 2016/day_04.py
@@ -0,0 +1,24 @@
+from collections import Counter
+from string import ascii_lowercase as abc
+from utils import read, print_answers
+
+
+def decypher(word, r):
+    return "".join(abc[(abc.index(c) + r) % len(abc)] for c in word)
+
+
+a1 = 0
+for line in read(2016, 4).split("\n"):
+    tmp, chk = line[:-1].split("[")
+    *words, rid = tmp.split("-")
+
+    decrypted = " ".join(decypher(w, int(rid)) for w in words)
+    if decrypted == "northpole object storage":
+        a2 = rid
+
+    c = Counter("".join(words))
+    _chk = "".join(sorted(c, key=lambda x: (c[x], -ord(x)), reverse=True)[:5])
+    if chk == _chk:
+        a1 += int(rid)
+
+print_answers(a1, a2, day=4)
