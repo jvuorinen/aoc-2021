584a47c4f1d50879b874327fc1d9dded39ad9396
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-01-23 16:49:35 +0200

    mods

--- 2016/day_04.py
+++ 2016/day_04.py
@@ -4,9 +4,8 @@ from utils import read, print_answers
 
 
 def is_legit(words, chk):
-    c = Counter("".join(words))
-    _chk = "".join(sorted(c, key=lambda x: (c[x], -ord(x)), reverse=True)[:5])
-    return chk == _chk
+    cnt = Counter("".join(words))
+    return chk == "".join(sorted(cnt, key=lambda x: (-cnt[x], ord(x)))[:5])
 
 
 def decypher(word, r):
@@ -18,11 +17,12 @@ for line in read(2016, 4).split("\n"):
     tmp, chk = line[:-1].split("[")
     *words, rid = tmp.split("-")
 
+    if is_legit(words, chk):
+        a1 += int(rid)
+
     decrypted = " ".join(decypher(w, int(rid)) for w in words)
     if decrypted == "northpole object storage":
         a2 = rid
 
-    if is_legit(words, chk):
-        a1 += int(rid)
 
-print_answers(a1, a2, day=4)
+print_answers(a1, a2, day=4)  # 173787 548
