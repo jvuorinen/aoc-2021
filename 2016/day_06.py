23f3cc8e05a99308ddbad45fe1a48953ca26f016
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-01-28 10:29:19 +0200

    cleaner

--- 2016/day_06.py
+++ 2016/day_06.py
@@ -5,4 +5,5 @@ msgs = read(2016, 6).split("\n")
 
 a1 = "".join([Counter(x).most_common()[0][0] for x in zip(*msgs)])
 a2 = "".join([Counter(x).most_common()[-1][0] for x in zip(*msgs)])
+
 print_answers(a1, a2, day=6)
