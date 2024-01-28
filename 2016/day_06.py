42999ccd6a369db75145c8ecc6739eea0e2d8972
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-01-28 10:31:13 +0200

    cleaner

--- 2016/day_06.py
+++ 2016/day_06.py
@@ -3,7 +3,8 @@ from utils import read, print_answers
 
 msgs = read(2016, 6).split("\n")
 
-a1 = "".join([Counter(x).most_common()[0][0] for x in zip(*msgs)])
-a2 = "".join([Counter(x).most_common()[-1][0] for x in zip(*msgs)])
+counted = [Counter(x).most_common() for x in zip(*msgs)]
+a1 = "".join([c[0][0] for c in counted])
+a2 = "".join([c[-1][0] for c in counted])
 
 print_answers(a1, a2, day=6)
