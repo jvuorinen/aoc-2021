0afabb16b905bd6e471943b9062e2b23c60a707e
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-11-25 08:30:18 +0200

    2024 folder

---
+++ 2024/template.py
@@ -0,0 +1,17 @@
+from itertools import combinations, permutations, count, cycle
+from functools import reduce, cache
+from collections import Counter, defaultdict
+from math import prod
+import numpy as np
+import re
+import networkx as nx
+from utils import read, print_answers
+
+raw = read().split("\n")
+# raw = read(2024, None).split("\n")
+
+
+a1 = None
+a2 = None
+
+print_answers(a1, a2, day=99)
