1136fda2e7a2e420115f0b49ba16b866f937823e
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-01 09:27:58 +0200

    cleaner

--- 2024/day_01.py
+++ 2024/day_01.py
@@ -3,9 +3,9 @@ from utils import print_answers, read
 
 raw = read(2024, 1)
 
-g1, g2 = map(
-    lambda lst: [int(x) for x in lst],
-    zip(*re.findall(r"(\d+)   (\d+)", raw)))
+ints = [int(x) for x in re.findall(r"\d+", raw)]
+g1 = ints[::2]
+g2 = ints[1::2]
 
 a1 = sum([abs(a - b) for a, b in zip(sorted(g1), sorted(g2))])
 a2 = sum([a * g2.count(a) for a in g1])
