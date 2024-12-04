ba1c18b682feba6fb8c132202a16b7e6dbf191ac
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-04 07:51:26 +0200

    cleaner

--- 2024/day_04.py
+++ 2024/day_04.py
@@ -1,29 +1,24 @@
 import numpy as np
 from utils import read, print_answers
 
-# raw = read().split("\n")
 raw = read(2024, 4).split("\n")
-
 arr = np.array([list(x) for x in raw])
 
-a1  = 0
+a1 = 0
 for _ in range(4):
     for line in arr:
-        txt = "".join(line)
-        a1 += txt.count("XMAS")
-    for i in range(0-len(arr), len(arr)):
-        txt = "".join(np.diag(arr, i))
-        a1 += txt.count("XMAS")
+        a1 += "".join(line).count("XMAS")
+    for i in range(-len(arr), len(arr)):
+        a1 += "".join(arr.diagonal(i)).count("XMAS")
     arr = np.rot90(arr)
 
 a2 = 0
 for i in range(len(arr) - 2):
-    for j in range(len(arr)-2):
-        slice = arr[i:i+3, j:j+3]
-        d1 = "".join(np.diag(slice))
-        d2 = "".join(np.diag(np.rot90(slice)))
+    for j in range(len(arr) - 2):
+        window = arr[i : i+3, j : j+3]
+        d1 = "".join(np.diag(window))
+        d2 = "".join(np.diag(np.rot90(window)))
         if d1 in ("SAM", "MAS") and d2 in ("SAM", "MAS"):
             a2 += 1
-
 
 print_answers(a1, a2, day=4)
