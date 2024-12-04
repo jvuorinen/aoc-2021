7405169da0f3c7b8fefc114c221b2bece112e0bb
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-04 08:28:46 +0200

    cleaner

--- 2024/day_03.py
+++ 2024/day_03.py
@@ -2,16 +2,16 @@ import re
 from utils import read, print_answers
 
 
-def calculate(program):
-    nums = re.findall(r"mul\((\d+),(\d+)\)", program)
+def calculate(prg):
+    nums = re.findall(r"mul\((\d+),(\d+)\)", prg)
     return sum(int(a) * int(b) for a, b in nums)
 
 
-def flipflop(s, state=True):
-    if (delim := "don't" if state else "do()") in s:
-        first, rest = s.split(delim, 1)
+def flipflop(prg, state=True):
+    if (delim := "don't" if state else "do()") in prg:
+        first, rest = prg.split(delim, 1)
         return flipflop(first, state) + flipflop(rest, not state)
-    return int(state) * calculate(s)
+    return int(state) * calculate(prg)
 
 
 program = read(2024, 3)
