d513325d777770e3c6ae7a2cc1b4b0c94592e905
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-05 09:42:22 +0200

    2016d19

--- 2016/day_19.py
+++ 2016/day_19.py
@@ -3,23 +3,22 @@ from utils import read, print_answers
 
 n = int(read(2016, 19))
 
-Q = deque(range(1, n+1))
+# Part 1
+Q = deque(range(1, n + 1))
 while len(Q) > 1:
     Q.rotate(-1)
     Q.popleft()
 a1 = Q.pop()
 
-# Slow
-Q = deque(range(1, n+1))
-while (lq := len(Q)) > 1:
-    if lq % 10000 == 0:
-        print(lq)
-    offset = len(Q)//2
-    Q.rotate(-offset)
-    Q.popleft()
-    Q.rotate(offset-1)
-a2 = Q.pop()
+# Part 2
+L = deque(range(1, n // 2 + 2))
+R = deque(range(n // 2 + 2, n + 1)[::-1])
+while len(R) > 0:
+    if len(L) == len(R):
+        L.append(R.pop())
+    L.pop()
+    R.appendleft(L.popleft())
+    L.append(R.pop())
+a2 = L.pop()
 
 print_answers(a1, a2, day=19)
-# 1816277
-# 1410967
