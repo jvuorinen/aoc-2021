c8c2eb364aa00c265a4ee80f2930ef6245b48b9f
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-05 21:51:46 +0200

    cleaner

--- 2016/day_20.py
+++ 2016/day_20.py
@@ -1,7 +1,7 @@
 from utils import read, print_answers
 
 raw = read(2016, 20).split("\n")
-blocks = [tuple(int(x) for x in b.split('-')) for b in raw]
+blocks = [tuple(int(x) for x in b.split("-")) for b in raw]
 
 valids = [(0, 4294967295)]
 for b in blocks:
@@ -17,6 +17,6 @@ for b in blocks:
     valids = _valids
 
 a1 = min(v[0] for v in valids)
-a2 = sum(v[1]-v[0] + 1 for v in valids)
+a2 = sum(v[1] - v[0] + 1 for v in valids)
 
 print_answers(a1, a2, day=20)
