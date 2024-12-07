c21a4edf5b0f28a0ba52932dc814d22f559043dd
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-07 10:28:55 +0200

    faster, cleaner

--- 2024/day_07.py
+++ 2024/day_07.py
@@ -1,35 +1,34 @@
 from itertools import product
-from tqdm import tqdm
 from utils import read, print_answers
 
 
-def check(line, ops):
-    _exp, _ns = line.split(': ')
-    exp = int(_exp)
-    ns = [int(x) for x in _ns.split(' ')]
+def _apply(val, op, x):
+    match op:
+        case "|":
+            return int(f"{val}{x}")
+        case "+":
+            return val + x
+        case "*":
+            return val * x
 
-    for cmb in product(*[list(ops) for _ in range(len(ns) - 1)]):
-        # print(cmb)
-        ints = ns.copy()
-        res = ints.pop(0)
+
+def check(line, ops):
+    _exp, _ns = line.split(": ")
+    exp = int(_exp)
+    ns = [int(x) for x in _ns.split(" ")]
+
+    for cmb in product(*[list(ops)] * (len(ns) - 1)):
+        val, rest = ns[0], ns[1:]
         for op in cmb:
-            x = ints.pop(0)
-            if op == '|':
-                res = int(f"{res}{x}")
-            else:
-                res = eval(f'res {op} {x}')
-        if res == exp:
-            # print("OO")
+            val = _apply(val, op, rest.pop(0))
+        if val == exp:
             return exp
     return 0
 
 
-lines = read().split("\n")
-# lines = read(2024, 7).split("\n")
+lines = read(2024, 7).split("\n")
 
-a1 = sum(check(x, '*+') for x in tqdm(lines))
-a2 = sum(check(x, '*+') or check(x, '*|+') for x in tqdm(lines))
+a1 = sum(check(x, "*+") for x in lines)
+a2 = sum(check(x, "*+") or check(x, "*|+") for x in lines)
 
 print_answers(a1, a2, day=7)
-# 3351424677624
-# 204976636995111
