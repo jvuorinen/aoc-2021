d24056c89cc2136d175d063b987f355763b85f2f
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-07 08:53:11 +0200

    cleaner

--- 2024/day_07.py
+++ 2024/day_07.py
@@ -4,30 +4,31 @@ from utils import read, print_answers
 
 
 def check(line, ops):
-    a, b = line.split(': ')
-    a = int(a)
-    b = b.split(' ')
-    size = len(b) - 1
+    _exp, _ns = line.split(': ')
+    exp = int(_exp)
+    ns = [int(x) for x in _ns.split(' ')]
 
-    for oc in product(*[list(ops) for _ in range(size)]):
-        ints = [int(x) for x in b]
+    for cmb in product(*[list(ops) for _ in range(len(ns) - 1)]):
+        # print(cmb)
+        ints = ns.copy()
         res = ints.pop(0)
-        for op in oc:
+        for op in cmb:
             x = ints.pop(0)
             if op == '|':
                 res = int(f"{res}{x}")
             else:
                 res = eval(f'res {op} {x}')
-        if res == a:
-            return a
+        if res == exp:
+            # print("OO")
+            return exp
     return 0
 
 
-# lines = read().split("\n")
-lines = read(2024, 7).split("\n")
+lines = read().split("\n")
+# lines = read(2024, 7).split("\n")
 
-a1 = sum(set(check(line, '+*') for line in tqdm(lines)))
-a2 = sum(set(check(line, '+*') or check(line, '+*|') for line in tqdm(lines)))
+a1 = sum(check(x, '*+') for x in tqdm(lines))
+a2 = sum(check(x, '*+') or check(x, '*|+') for x in tqdm(lines))
 
 print_answers(a1, a2, day=7)
 # 3351424677624
