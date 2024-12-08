713ff0d30b3ea58866b67a1291d4d39848c57e9a
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-08 18:51:02 +0200

    fix

--- 2016/day_21.py
+++ 2016/day_21.py
@@ -1,22 +1,10 @@
-from itertools import combinations, permutations, count, cycle
-from functools import reduce, cache
-from collections import Counter, defaultdict, deque
-from math import prod
-import numpy as np
+from itertools import permutations
+from functools import reduce
 from re import findall
-import networkx as nx
 from utils import read, print_answers
 
-lines = read().split("\n")
-# lines = read(2016, 21).split("\n")
 
-original = 'abcdefgh'
-# original = "abcde"
-
-NROT = {i: (-(i + 1 + int(i >= 4)) % len(original)) for i in range(len(original))}
-# INVROT = {k: v for k, v in NROT.items()}
-
-def fumble(lst, line):
+def _step(lst, line):
     lst = lst.copy()
     if "swap position" in line:
         a, b = map(int, findall(r"\d", line))
@@ -30,32 +18,28 @@ def fumble(lst, line):
         lst[a:b+1] = lst[a:b+1][::-1]
     elif "move" in line:
         a, b = map(int, findall(r"\d", line))
-        # if invert:
-        #     a, b = b, a
         lst.insert(b, lst.pop(a))
     elif "rotate" in line:
         if "based" in line:
             a = findall(r"letter (.)", line)[0]
             ia = lst.index(a)
-            n = NROT[ia]
+            n = -(1 + ia + (ia >= 4))
         else:
             n = int(findall(r"(.) step", line)[0])
             if "right" in line:
                 n *= -1
-            # if invert:
-            #     n *= -1
-            n %= len(lst)
+        n %= len(lst)
         lst = lst[n:] + lst[:n]
     return lst
 
-invert = False
-"".join(reduce(fumble, lines, list(original)))
 
-invert = True
-"".join(reduce(fumble, lines, list(original)))
+def fumble(text):
+    return "".join(reduce(_step, lines, list(text)))
 
-a1 = "".join(reduce(fumble, lines, list(original)))
 
-# a2 = "".join(["fbgdceah"[i] for i in ixs])
+lines = read(2016, 21).split("\n")
 
-# print_answers(a1, a2, day=21)
+a1 = fumble('abcdefgh')
+a2 = next("".join(p) for p in permutations('abcdefgh') if (fmb := fumble(p)) == 'fbgdceah')
+
+print_answers(a1, a2, day=21)
