de37261fa073762b8fce99b38d6db296bbf367af
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-10 17:34:13 +0200

    refactor

--- 2024/day_09.py
+++ 2024/day_09.py
@@ -1,8 +1,17 @@
 from utils import read, print_answers
 
 
+def checksum(data):
+    return sum(i * (x or 0) for i, x in enumerate(data))
+
+
+def unfold(blocks):
+    nested = [[i // 2 if i % 2 == 0 else None] * b[1] for i, b in enumerate(blocks)]
+    return [x for xs in nested for x in xs]
+
+
 def solve1(data):
-    data = data.copy()
+    data = unfold(blocks)
     i, ii = 0, len(data) - 1
     while True:
         while data[i] is not None:
@@ -10,46 +19,38 @@ def solve1(data):
         while data[ii] is None:
             ii -= 1
         if ii <= i:
-            return sum(i * (x or 0) for i, x in enumerate(data))
+            return checksum(data)
         data[i], data[ii] = data[ii], data[i]
 
 
-def solve2(data, idx_file, idx_empty, empty):
-    data = data.copy()
-    empty_areas = [(ie, len(e)) for ie, e in zip(idx_empty, empty) if e]
+def solve2(blocks):
+    data = unfold(blocks)
+    files = [*enumerate(blocks[::2])]
+    empty = [b for b in blocks[1::2] if b[1]]
 
-    for ii, f in zip(idx_file[::-1], files[::-1]):
-        s = len(f)
-        for ie, (i, es) in enumerate(empty_areas):
+    for f_id, (ii, sf) in files[::-1]:
+        for ie, (i, se) in enumerate(empty):
             if i >= ii:
                 break
-            if es < s:
+            if se < sf:
                 continue
-            data[i : i + s] = f
-            data[ii : ii + s] = [None] * s
-            empty_areas.pop(ie)
-            if ns := es - s:
-                empty_areas.insert(ie, (i + s, ns))
+            data[i : i + sf] = [f_id] * sf
+            data[ii : ii + sf] = [None] * sf
+            empty.pop(ie)
+            if ns := se - sf:
+                empty.insert(ie, (i + sf, ns))
             break
-
-    return sum(i * (x or 0) for i, x in enumerate(data))
+    return checksum(data)
 
 
 raw = read(2024, 9)
 
-files = [int(c) * [i] for i, c in enumerate(raw[::2])]
-empty = [int(c) * [None] for c in raw[1::2]]
+i, blocks = 0, []
+for x in list(raw):
+    blocks += [(i, s := int(x))]
+    i += s
 
-i = 0
-data, idx_file, idx_empty = [], [], []
-for f, e in zip(files, empty + [[None]]):
-    data.extend(f)
-    data.extend(e)
-    idx_file.append(i)
-    idx_empty.append(i := i + len(f))
-    i += len(e)
-
-a1 = solve1(data)
-a2 = solve2(data, idx_file, idx_empty, empty)
+a1 = solve1(blocks)
+a2 = solve2(blocks)
 
 print_answers(a1, a2, day=9)
