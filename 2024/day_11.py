c050b5849ac6ae6ccfae6ce95871f3c8dc45189c
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-11 07:25:25 +0200

    d11

--- 2024/day_11.py
+++ 2024/day_11.py
@@ -5,18 +5,17 @@ raw = read(2024, 11)
 stones = [*map(int, raw.split())]
 
 @cache
-def solve(s, reps):
+def solve(stone, reps):
     if reps == 0:
         return 1
     nxt = []
-
-    if s == 0:
+    if stone == 0:
         nxt.append(1)
-    elif len(ss := str(s)) % 2 == 0:
+    elif len(ss := str(stone)) % 2 == 0:
         nxt.append(int(ss[:len(ss) // 2]))
         nxt.append(int(ss[len(ss) // 2:]))
     else:
-        nxt.append(s * 2024)
+        nxt.append(stone * 2024)
     return sum(solve(n, reps - 1) for n in nxt)
 
 a1 = sum([solve(x, 25) for x in stones])
