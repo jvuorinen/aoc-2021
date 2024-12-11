757a2ad434028f9932bb9fd6648f3450f4271a95
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-11 07:23:09 +0200

    d11

--- 2024/day_11.py
+++ 2024/day_11.py
@@ -1,18 +1,25 @@
-from itertools import combinations, permutations, product, count, cycle
-from functools import reduce, cache
-from collections import Counter, defaultdict, deque
-from math import prod
-import numpy as np
-from re import findall
-import networkx as nx
-from tqdm import tqdm
+from functools import cache
 from utils import read, print_answers
 
-raw = read().split("\n")
-# raw = read(2024, 11).split("\n")
+raw = read(2024, 11)
+stones = [*map(int, raw.split())]
 
+@cache
+def solve(s, reps):
+    if reps == 0:
+        return 1
+    nxt = []
 
-a1 = None
-a2 = None
+    if s == 0:
+        nxt.append(1)
+    elif len(ss := str(s)) % 2 == 0:
+        nxt.append(int(ss[:len(ss) // 2]))
+        nxt.append(int(ss[len(ss) // 2:]))
+    else:
+        nxt.append(s * 2024)
+    return sum(solve(n, reps - 1) for n in nxt)
+
+a1 = sum([solve(x, 25) for x in stones])
+a2 = sum([solve(x, 75) for x in stones])
 
 print_answers(a1, a2, day=11)
