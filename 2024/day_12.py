b8702b0e12e1e09a6a6eb1ccbb1c59c226a054b5
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-12 09:51:13 +0200

    cleaner

--- 2024/day_12.py
+++ 2024/day_12.py
@@ -1,38 +1,35 @@
 from itertools import combinations, permutations
 from utils import read, print_answers
 
-# raw = read().split("\n")
 raw = read(2024, 12).split("\n")
 
 M = {c - 1j * r: x for r, line in enumerate(raw) for c, x in enumerate(line)}
 
 regions = []
-todo = set(M)
-while todo:
-    z = todo.pop()
-    c = M.pop(z)
-
-    r = set()
-    buf = [z]
-    while buf:
-        z = buf.pop()
-        r.add(z)
-        ns = [nz for d in [1, -1, 1j, -1j] if M.get(nz := z + d) == c and nz not in r]
-        todo -= set(ns)
-        buf += ns
-    regions.append(r)
+while M:
+    z, c = M.popitem()
+    reg = set()
+    todo = {z}
+    while todo:
+        z = todo.pop()
+        reg.add(z)
+        ns = set(_z for d in [1, -1, 1j, -1j] if (M.get(_z := z + d) == c) and _z not in reg)
+        for n in ns:
+            todo.add(n)
+            M.pop(n)
+    regions.append(reg)
 
 a1 = a2 = 0
-for r in regions:
-    adj = [p for p in combinations(r, 2) if abs(p[0] - p[1]) == 1]
-    perim = 4 * len(r) - 2 * len(adj)
-    a1 += len(r) * perim
+for reg in regions:
+    adj = [p for p in combinations(reg, 2) if abs(p[0] - p[1]) == 1]
+    perim = 4 * len(reg) - 2 * len(adj)
+    a1 += len(reg) * perim
 
     corr = sum(
         1
-        for a, b in permutations(r, 2)
-        if abs(d := a - b) == 1 and (a + (1j * d) not in r) and (b + (1j * d) not in r)
+        for a, b in permutations(reg, 2)
+        if abs(d := a - b) == 1 and (a + (1j * d) not in reg) and (b + (1j * d) not in reg)
     )
-    a2 += len(r) * (perim - corr)
+    a2 += len(reg) * (perim - corr)
 
 print_answers(a1, a2, day=12)
