53d5390ff9a266dcdc5dcd7aec51d167f1c7ec9f
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-12 10:20:37 +0200

    cleaner

--- 2024/day_12.py
+++ 2024/day_12.py
@@ -1,35 +1,31 @@
-from itertools import combinations, permutations
+from itertools import permutations
 from utils import read, print_answers
 
 raw = read(2024, 12).split("\n")
-
 M = {c - 1j * r: x for r, line in enumerate(raw) for c, x in enumerate(line)}
 
+# Get regions via flood fill
 regions = []
 while M:
     z, c = M.popitem()
-    reg = set()
+    rg = set()
     todo = {z}
     while todo:
         z = todo.pop()
-        reg.add(z)
-        ns = set(_z for d in [1, -1, 1j, -1j] if (M.get(_z := z + d) == c) and _z not in reg)
-        for n in ns:
+        rg.add(z)
+        for n in set(_z for d in [1, -1, 1j, -1j] if (M.get(_z := z + d) == c) and _z not in rg):
             todo.add(n)
             M.pop(n)
-    regions.append(reg)
+    regions.append(rg)
 
+# Calculations
 a1 = a2 = 0
-for reg in regions:
-    adj = [p for p in combinations(reg, 2) if abs(p[0] - p[1]) == 1]
-    perim = 4 * len(reg) - 2 * len(adj)
-    a1 += len(reg) * perim
+for rg in regions:
+    adj = [p for p in permutations(rg, 2) if abs(p[0] - p[1]) == 1]
+    perim = 4 * len(rg) - 2 * (len(adj) // 2)
+    a1 += len(rg) * perim
 
-    corr = sum(
-        1
-        for a, b in permutations(reg, 2)
-        if abs(d := a - b) == 1 and (a + (1j * d) not in reg) and (b + (1j * d) not in reg)
-    )
-    a2 += len(reg) * (perim - corr)
+    corr = sum(1 for a, b in adj if not rg & {a + (1j * (a - b)), b + (1j * (a - b))})
+    a2 += len(rg) * (perim - corr)
 
 print_answers(a1, a2, day=12)
