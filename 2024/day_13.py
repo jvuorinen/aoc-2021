60f8fd720b0e49d83455b32252cdca72992d86ef
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-13 08:01:06 +0200

    d13 works

--- 2024/day_13.py
+++ 2024/day_13.py
@@ -1,18 +1,42 @@
-from itertools import combinations, permutations, product, count, cycle
-from functools import reduce, cache
-from collections import Counter, defaultdict, deque
-from math import prod
 import numpy as np
 from re import findall
-import networkx as nx
-from tqdm import tqdm
 from utils import read, print_answers
 
-raw = read().split("\n")
-# raw = read(2024, 13).split("\n")
+# raw = read().split("\n\n")
+raw = read(2024, 13).split("\n\n")
+
+xs, ys = [], []
+for line in raw:
+    nums = [*map(int, findall(r'\d+', line))]
+    xs.append(np.array([nums[0:2], nums[2:4]]).T)
+    ys.append(np.array([nums[4:]]).T)
+
+funny, results = list(), list()
+
+a1 = 0
+for x, y in zip(xs, ys):
+    y += 10000000000000
+    res = np.linalg.solve(x, y)
+    a = res[0][0]
+    b = res[1][0]
+
+    # tst =  x[1] / x[0]
+    # if tst[0] == tst[1] :
+    #     print(x)
+    # print()
+    # print(x, y, res)
+
+    tst = x @ res.round()
+    if all(tst == y):
+        a1 += round(3*a) + round(b)
+        results.append(res)
+    else:
+        funny.append(res)
+        # print(res)
+    # np.linalg.solve()
 
 
-a1 = None
 a2 = None
 
 print_answers(a1, a2, day=13)
+# 32026
