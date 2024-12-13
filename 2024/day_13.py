449b6674cfdf93a115641c44b38f2ebd98d5f6f7
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-13 08:13:11 +0200

    d13

--- 2024/day_13.py
+++ 2024/day_13.py
@@ -1,42 +1,20 @@
-import numpy as np
 from re import findall
+import numpy as np
 from utils import read, print_answers
 
-# raw = read().split("\n\n")
 raw = read(2024, 13).split("\n\n")
 
-xs, ys = [], []
+ans = [0, 0]
 for line in raw:
     nums = [*map(int, findall(r'\d+', line))]
-    xs.append(np.array([nums[0:2], nums[2:4]]).T)
-    ys.append(np.array([nums[4:]]).T)
 
-funny, results = list(), list()
+    X = np.array([nums[0:2], nums[2:4]]).T
+    y = np.array([nums[4:]]).T
 
-a1 = 0
-for x, y in zip(xs, ys):
-    y += 10000000000000
-    res = np.linalg.solve(x, y)
-    a = res[0][0]
-    b = res[1][0]
-
-    # tst =  x[1] / x[0]
-    # if tst[0] == tst[1] :
-    #     print(x)
-    # print()
-    # print(x, y, res)
-
-    tst = x @ res.round()
-    if all(tst == y):
-        a1 += round(3*a) + round(b)
-        results.append(res)
-    else:
-        funny.append(res)
-        # print(res)
-    # np.linalg.solve()
+    for i, y in enumerate([y, y + 10000000000000]):
+        res = np.linalg.solve(X, y).round().astype(int)
+        if all(X @ res == y):
+            ans[i] += np.dot(res.T, [3, 1])[0]
 
 
-a2 = None
-
-print_answers(a1, a2, day=13)
-# 32026
+print_answers(ans[0], ans[1], day=13)
