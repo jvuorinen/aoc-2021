936a7ccdef506305a06c596a6c35bad6c1f929e8
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-14 10:11:44 +0200

    fix

--- 2024/day_14.py
+++ 2024/day_14.py
@@ -5,15 +5,11 @@ from utils import read, print_answers
 
 
 W, H = 101, 103
-Q = [*product([(0, W // 2), (W // 2 + 2, W)], [(0, H // 2), (H // 2 + 2, H)])]
+Q = [*product(*[[(0, d // 2 - 1), (d // 2 + 1, d)] for d in (W, H)])]
 
-
-raw = read(2024, 14).split("\n")
-ps, vs = [], []
-for line in raw:
-    x, y, vx, vy = map(int, findall(r"\-?\d+", line))
-    ps.append(x + 1j * y)
-    vs.append(vx + 1j * vy)
+raw = read(2024, 14)
+ints = [map(int, findall(r"\-?\d+", line)) for line in raw.split("\n")]
+ps, vs = zip(*[(x + 1j * y, vx + 1j * vy) for x, y, vx, vy in ints])
 
 
 def simulate(ps, vs, n):
@@ -25,20 +21,19 @@ def in_bounds(z, br1, br2, bi1, bi2):
     return (br1 <= z.real <= br2) and (bi1 <= z.imag <= bi2)
 
 
-def get_quadrants_counts(robots):
-    return [sum(in_bounds(r, a, b, c, d) for r in robots) for (a, b), (c, d) in Q]
+def get_quadrants_counts(ps):
+    return [sum(in_bounds(z, a, b, c, d) for z in ps) for (a, b), (c, d) in Q]
 
 
 def draw(ps):
-    area = [["#" if (x + y*1j in ps) else "." for x in range(W)] for y in range(H)]
+    area = [["#" if (x + y * 1j in ps) else "." for x in range(W)] for y in range(H)]
     print("\n".join(map("".join, area)))
 
 
 a1 = prod(get_quadrants_counts(simulate(ps, vs, 100)))
 
-# tests = [get_quadrants_counts(simulate(ps, vs, i)) for i in range(10_000)]
-# a2, _ = max(enumerate(tests), key=lambda x: max(x[1]))
-a2 = None
+tests = [get_quadrants_counts(simulate(ps, vs, i)) for i in range(10_000)]
+a2, _ = max(enumerate(tests), key=lambda x: max(x[1]))
 
 # draw(simulate(ps, vs, a2))
 print_answers(a1, a2, day=14)
