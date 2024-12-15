2e5259701306776f3831d3ae7b86620da1e1dee5
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-15 12:14:50 +0200

    cleaner

--- 2024/day_15.py
+++ 2024/day_15.py
@@ -1,104 +1,59 @@
-from itertools import combinations, permutations, product, count, cycle
-from functools import reduce, cache
-from collections import Counter, defaultdict, deque
-from math import prod
-import numpy as np
-from re import findall
-import networkx as nx
-from tqdm import tqdm
 from utils import read, print_answers
 
 # a, b = read().split("\n\n")
-a, b = read(2024, 15).split("\n\n")
-
-
-a = a.replace("#", "##")
-a = a.replace("O", "[]")
-a = a.replace(".", "..")
-a = a.replace("@", "@.")
-
-M = {c + 1j * r: x for r, line in enumerate(a.split('\n')) for c, x in enumerate(line)}
-W, H = int(max(x.real for x in M)) + 1, int(max(x.imag for x in M)) + 1
-
+area, b = read(2024, 15).split("\n\n")
 dirs = [{'<': -1, '>': 1, '^': -1j, 'v': 1j}[x] for x in b.replace("\n", "")]
 
+def solve(area, part2=False):
+    if part2:
+        area = area.replace("#", "##")
+        area = area.replace("O", "[]")
+        area = area.replace(".", "..")
+        area = area.replace("@", "@.")
 
-p = next(k for k, v in M.items() if v == '@')
-barrels = set([k for k, v in M.items() if v == '['])
-floor = set([k for k, v in M.items() if v != '#'])
+    M = {c + 1j * r: x for r, line in enumerate(area.split('\n')) for c, x in enumerate(line)}
 
-def draw():
-    area = [list('#'* W) for _ in range(H)]
-    for z in floor:
-        area[int(z.imag)][int(z.real)] = '.'
-    for z in barrels:
-        area[int(z.imag)][int(z.real)] = '['
-        area[int((z+1).imag)][int((z+1).real)] = ']'
-    area[int(p.imag)][int(p.real)]= '@'
-    print("\n".join(map("".join, area)))
-    print()
+    p = next(k for k, v in M.items() if v == '@')
+    barrels = set([k for k, v in M.items() if v == '['])
+    floor = set([k for k, v in M.items() if v != '#'])
 
-def get_stack(p, d, stack):
-    if p - 1 in barrels:
-        # print("apply p correction")
-        p -= 1
-    if p not in barrels:
-        return
-    # print("Checking ", p)
-    _p = p + d
-    if (_p not in floor) or (_p + 1 not in floor):
-        # print(_p, " blocked")
-        stack.clear()
-        return
-    # print(p, " is free")
-    stack.add(p)
-    if d in (1, -1):
-        if stack and _p + d in barrels:
-            get_stack(_p + d, d, stack)
-    else:
-        if stack and _p - 1 in barrels:
-            get_stack(_p - 1, d, stack)
-        if stack and _p in barrels:
-            get_stack(_p, d, stack)
-        if stack and _p + 1 in barrels:
-            get_stack(_p + 1, d, stack)
-    # print(stack)
-    return stack or None
+    def _get_stack(p, d, stack):
+        if part2 and p - 1 in barrels:
+            p -= 1
+        if p not in barrels:
+            return
+        _p = p + d
+        if (_p not in floor) or (_p + 1 not in floor):
+            stack.clear()
+            return
+        stack.add(p)
 
-# draw()
-# get_stack(p, 1, set())
-# draw()
+        if d in (1, -1):
+            if stack and _p + d in barrels:
+                _get_stack(_p + d, d, stack)
+        else:
+            if stack and _p - 1 in barrels:
+                _get_stack(_p - 1, d, stack)
+            if stack and _p in barrels:
+                _get_stack(_p, d, stack)
+            if stack and _p + 1 in barrels:
+                _get_stack(_p + 1, d, stack)
+        return stack or None
 
-pushes = []
-
-while dirs:
-    d = dirs.pop(0)
-    if p + d in floor and (p+d) not in barrels and (p+d-1) not in barrels:
-        # print({-1: "<", 1: ">", -1j: "^", 1j: "v"}[d])
-        p += d
-    else:
-        stack = get_stack(p+d, d, set())
-        if stack:
-            # print("---------")
-            # draw()
-            # print({-1: "<", 1: ">", -1j: "^", 1j: "v"}[d])
-            # state = (p, barrels)
-            # pushes.append
-            
-            barrels -= stack
-            barrels |= {b + d for b in stack}
+    for d in dirs:
+        if p + d in floor and (p+d) not in barrels and (p+d-1) not in barrels:
             p += d
-            # draw()
-            # break
-    # if p == 6 + 6j:
-    #     print(len(dirs))
-    #     break
-# draw()
+        else:
+            stack = _get_stack(p+d, d, set())
+            if stack:
+                barrels -= stack
+                barrels |= {b + d for b in stack}
+                p += d
 
+    return int(sum([100*z.imag + z.real for z in barrels]))
 
-a1 = int(sum([100*z.imag + z.real for z in barrels]))
-a2 = None
-
+a1 = None
+a2 = solve(area, part2=True)
 print_answers(a1, a2, day=15)
 # 1426855
 # 1404917
