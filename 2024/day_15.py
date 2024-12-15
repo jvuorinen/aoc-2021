1d020719e1dd0d04e665f0830943d52a6a4d8420
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-15 14:18:47 +0200

    done

--- 2024/day_15.py
+++ 2024/day_15.py
@@ -4,10 +4,11 @@ area, _dirs = read(2024, 15).split("\n\n")
 dirs = [{"<": -1, ">": 1, "^": -1j, "v": 1j}[x] for x in _dirs.replace("\n", "")]
 
 
-def has_box(p, boxes, part2):
-    if not part2:
-        return p in boxes
-    return p in boxes or (p - 1) in boxes
+def get_box(p, boxes, part2):
+    if p in boxes:
+        return p
+    if part2 and (p-1) in boxes:
+        return (p-1)
 
 
 def descendants(p, d, boxes, part2):
@@ -18,19 +19,19 @@ def descendants(p, d, boxes, part2):
     return [_p for dd in (-1, 0, 1) if (_p := p + d + dd) in boxes] or []
 
 
-def build_stack(p, d, boxes, floor, stack, part2):
+def build_stack(p, d, boxes, walls, stack, part2):
     if part2 and p - 1 in boxes:
         p -= 1
     if p not in boxes:
         return
     _p = p + d
-    if (_p not in floor) or (part2 and (_p + 1 not in floor)):
+    if (_p in walls) or (part2 and (_p + 1 in walls)):
         stack.clear()
         return
     stack.add(p)
     for _p in descendants(p, d, boxes, part2):
         if stack:
-            build_stack(_p, d, boxes, floor, stack, part2)
+            build_stack(_p, d, boxes, walls, stack, part2)
     return stack or None
 
 
@@ -44,17 +45,20 @@ def solve(area, part2=False):
     M = {c + 1j * r: x for r, line in enumerate(area.split("\n")) for c, x in enumerate(line)}
     p = next(k for k, v in M.items() if v == "@")
     boxes = set([k for k, v in M.items() if v in "O["])
-    floor = set([k for k, v in M.items() if v != "#"])
+    walls = set([k for k, v in M.items() if v == "#"])
 
     for d in dirs:
-        if p + d in floor and not has_box(p + d, boxes, part2):
-            p += d
-        else:
-            stack = build_stack(p + d, d, boxes, floor, set(), part2)
+        _p = p + d
+        if _p in walls:
+            continue
+        elif b := get_box(_p, boxes, part2):
+            stack = build_stack(b, d, boxes, walls, set(), part2)
             if stack:
                 boxes -= stack
                 boxes |= {b + d for b in stack}
-                p += d
+                p = _p
+        else:
+            p = _p
 
     return int(sum([100 * z.imag + z.real for z in boxes]))
 
@@ -62,5 +66,3 @@ def solve(area, part2=False):
 a1 = solve(area, part2=False)
 a2 = solve(area, part2=True)
 print_answers(a1, a2, day=15)
-# 1426855
-# 1404917
