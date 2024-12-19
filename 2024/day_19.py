33ce38b85a66f3d4566269f23f6995dfe6d62138
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-19 09:56:35 +0200

    d19

--- 2024/day_19.py
+++ 2024/day_19.py
@@ -1,41 +1,24 @@
-from itertools import combinations, permutations, product, count, cycle
-from functools import reduce, cache
-from collections import Counter, defaultdict, deque
-from math import prod
-import numpy as np
-from re import finditer, findall
-import networkx as nx
-from tqdm import tqdm
+from functools import cache
 from utils import read, print_answers
 
-# raw = read()
-raw = read(2024, 19)
 
-a, b = raw.split('\n\n')
-pats = a.split(', ')
-mats = b.split('\n')
-
-def get_pattern(ps):
-    pgs = "|".join(f"{p}" for p in ps)
-    return f"^({pgs})(.*)$"
+_p, _m = read(2024, 19).split("\n\n")
+pats = _p.split(", ")
+mats = _m.split("\n")
 
 
 @cache
 def matches(mat, s=0):
     if len(mat) == 0:
         return 1
-    ps = set(pats)
-    while (match := findall(get_pattern(ps), mat)):
-        a, b = match[0]
-        ps.remove(a)
-        s += matches(b)
+    for p in [p for p in pats if mat.startswith(p)]:
+        s += matches(mat[len(p) :])
     return s
 
-nways = [matches(m) for m in tqdm(mats)]
+
+nways = [matches(m) for m in mats]
 
 a1 = sum(x > 0 for x in nways)
 a2 = sum(nways)
 
 print_answers(a1, a2, day=19)
-# 315
-# 625108891232249
