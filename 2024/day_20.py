65bcc42bf0e10d2f1c81d5cb9ef6f80359a38ad5
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-20 17:28:29 +0200

    cleaner

--- 2024/day_20.py
+++ 2024/day_20.py
@@ -1,43 +1,46 @@
-from itertools import permutations
 from utils import read, print_answers
 
 
-def bfs(floor):
-    floor = floor.copy()
+def get_distances(area):
+    floor = set([k for k, v in area.items() if v != "#"])
+
     todo = [(0, start)]
-    dist = {}
+    steppable = floor.copy()
+    P = {}
     while todo:
         i, n = todo.pop(0)
-        dist[n] = i
-        floor -= {n}
-        todo += [(i + 1, nn) for d in (1, -1, 1j, -1j) if (nn := n + d) in floor]
-    return dist
+        P[n] = i
+        steppable -= {n}
+        todo += [(i + 1, nn) for d in (1, -1, 1j, -1j) if (nn := n + d) in steppable]
+
+    L1 = {
+        (f, ff): abs(r) + abs(i)
+        for r in range(-20, 20 + 1)
+        for i in range(-(20 - abs(r)), 20 - abs(r) + 1)
+        for f in floor
+        if (ff := f + r + 1j * i) in floor
+    }
+    return P, L1
 
 
-def l1(a, b):
-    return int(abs((a - b).real) + abs((a - b).imag))
-
-
-def get_dist(w1, w2):
-    return D[w1] + l1(w1, w2) + D[end] - D[w2]
+def length_with_cheat(c1, c2):
+    cheat = abs((c1 - c2).real) + abs((c1 - c2).imag)
+    return P[c1] + cheat + P[end] - P[c2]
 
 
 def solve(time, thrs):
-    warps = [w for w in permutations(floor, 2) if 1 < l1(*w) <= time]
-    distances = [d for w in warps if (d := get_dist(*w))]
-    return len([d for d in distances if D[end] - d >= thrs])
+    cheats = [c for c, d in L1.items() if 1 < d <= time]
+    distances = [d for c in cheats if (d := length_with_cheat(*c))]
+    return len([d for d in distances if P[end] - d >= thrs])
 
 
-raw = read(2024, 20).split("\n")
+raw = read(2024, 20)
+area = {c + 1j * r: x for r, line in enumerate(raw.split("\n")) for c, x in enumerate(line)}
+start = next(k for k, v in area.items() if v == "S")
+end = next(k for k, v in area.items() if v == "E")
 
-M = {c + 1j * r: x for r, line in enumerate(raw) for c, x in enumerate(line)}
-start = next(k for k, v in M.items() if v == "S")
-end = next(k for k, v in M.items() if v == "E")
-floor = set([k for k, v in M.items() if v != "#"])
-
-D = bfs(floor)
+P, L1 = get_distances(area)
 
 a1 = solve(time=2, thrs=100)
 a2 = solve(time=20, thrs=100)
-
 print_answers(a1, a2, day=20)
