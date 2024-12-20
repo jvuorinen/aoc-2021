0b20750d768900b55edb23092f98ed633e871f33
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-20 11:45:59 +0200

    d20

--- 2024/day_20.py
+++ 2024/day_20.py
@@ -1,70 +1,43 @@
-from itertools import combinations, permutations, product, count, cycle
-from functools import reduce, cache
-from collections import Counter, defaultdict, deque
-from math import prod
-import numpy as np
-from re import findall
-import networkx as nx
-from tqdm import tqdm
+from itertools import permutations
 from utils import read, print_answers
 
-# raw = read().split("\n")
-raw = read(2024, 20).split("\n")
-M = {c + 1j * r: x for r, line in enumerate(raw) for c, x in enumerate(line)}
-
-
-def get_next(i, x, n, d):
-    nxt = []
-    for j in (1, 1j, -1j):
-        if M.get(nn := (n + j*d), 'x') in '.E':
-            nxt.append((i+1, x, nn, j*d))
-        if x and M.get(nn := (n + 2*j*d), 'x') in '.E':
-            nxt.append((i+2, x-1, nn, j*d))
-    return nxt
-
-
-# def bfs(x):
-#     n = next(k for k, v in M.items() if v == 'S')
-#     d = next(d for d in (1, -1, -1j, 1j) if M.get(n+d) == '.')
-#     end = next(k for k, v in M.items() if v == 'E')
-
-#     best = {}
-#     todo = [(0, x, n, d)]
-#     res = []
-#     while todo:
-#         # print(todo)
-#         i, x, n, d = todo.pop(0)
-#         if n == end:
-#             res.append(i)
-#         else:
-#             for nxt in get_next(i, x, n, d):
-#                 ii, xx, nn, dd = nxt
-#                 if nn != end and best.get(nn, (0, 1e9)) > (-xx, ii):
-#                     todo.append(nxt)
-#                     best[nn] = (-xx, ii)
-#     return res
-
-start = next(k for k, v in M.items() if v == 'S')
-end = next(k for k, v in M.items() if v == 'E')
-floor = set([k for k, v in M.items() if v != '#'])
-walls = set([k for k, v in M.items() if v == '#'])
 
 def bfs(floor):
     floor = floor.copy()
     todo = [(0, start)]
+    dist = {}
     while todo:
         i, n = todo.pop(0)
+        dist[n] = i
         floor -= {n}
-        if n == end:
-            return i
-        todo += [(i+1, nn) for d in (1, -1, 1j, -1j) if (nn := n+d) in floor]
+        todo += [(i + 1, nn) for d in (1, -1, 1j, -1j) if (nn := n + d) in floor]
+    return dist
 
-ref = bfs(floor)
 
-res = [bfs(floor | {w}) for w in tqdm(walls)]
+def l1(a, b):
+    return int(abs((a - b).real) + abs((a - b).imag))
 
-a1 = len([x for x in res if ref - x >= 100])
 
-a2 = None
+def get_dist(w1, w2):
+    return D[w1] + l1(w1, w2) + D[end] - D[w2]
+
+
+def solve(time, thrs):
+    warps = [w for w in permutations(floor, 2) if 1 < l1(*w) <= time]
+    distances = [d for w in warps if (d := get_dist(*w))]
+    return len([d for d in distances if D[end] - d >= thrs])
+
+
+raw = read(2024, 20).split("\n")
+
+M = {c + 1j * r: x for r, line in enumerate(raw) for c, x in enumerate(line)}
+start = next(k for k, v in M.items() if v == "S")
+end = next(k for k, v in M.items() if v == "E")
+floor = set([k for k, v in M.items() if v != "#"])
+
+D = bfs(floor)
+
+a1 = solve(time=2, thrs=100)
+a2 = solve(time=20, thrs=100)
 
 print_answers(a1, a2, day=20)
