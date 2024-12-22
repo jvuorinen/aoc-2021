a8f83dd52fb672622c9c861e46b6b7cb53f3198d
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-22 20:19:59 +0200

    2016 stuff

--- 2016/day_23.py
+++ 2016/day_23.py
@@ -1,41 +1,41 @@
 from collections import defaultdict
 from utils import read, print_answers
 
-
-def parse(raw):
-    program = []
-    for line in raw:
-        cmd, *args = line.split(" ")
+def run(program):
+    mem = defaultdict(int)
+    while mem["i"] < len(program):
+        cmd, *args = program[mem["i"]].split(" ")
+        # print(cmd, args, mem)
         match cmd:
             case "cpy":
-                program.append(f"{args[1]} = {args[0]}; i += 1")
+                exec(f"{args[1]} = {args[0]}; i += 1", {}, mem)
             case "inc":
-                program.append(f"{args[0]} += 1; i += 1")
+                exec(f"{args[0]} += 1; i += 1", {}, mem)
             case "dec":
-                program.append(f"{args[0]} -= 1; i += 1")
+                exec(f"{args[0]} -= 1; i += 1", {}, mem)
             case "jnz":
-                program.append(f"i += 1 if {args[0]} == 0 else {args[1]}")
-    return program
-
-
-def run(program, part2=False):
-    mem = defaultdict(int)
-    if part2:
-        mem["c"] = 1
-    while mem["i"] < len(program):
-        cmd = program[mem["i"]]
-        if mem["i"] == 10:
-            shortcut = mem["b"] - 1
-            mem["a"] += shortcut
-            mem["b"] -= shortcut
-        exec(cmd, {}, mem)
+                exec(f"i += 1 if {args[0]} == 0 else {args[1]}",{},  mem)
+            case "tgl":
+                ti = mem["i"] + eval(args[0], {}, mem)
+                # break
+                if 0 <= ti < len(program):
+                    tc, *targs = program[ti].split(" ")
+                    if len(targs) == 1:
+                        ct = "dec" if tc == "inc" else "inc"
+                        program[ti] = f"{ct} {targs[0]}"
+                    else:
+                        ct = "cpy" if tc == "jnz" else "jnz"
+                        program[ti] = f"{ct} {targs[0]} {targs[1]}"
+                    # break
+                mem["i"] += 1
     return mem["a"]
 
+program = read().split("\n")
+# program = read(2016, 23).split("\n")
+# run(program)
 
-raw = read(2016, 12).split("\n")
-program = parse(raw)
 
-a1 = run(program)
-a2 = run(program, part2=True)
+# a1 = run(program)
+# a2 = run(program, part2=True)
 
-print_answers(a1, a2, day=23)  # 318020 9227674
+# print_answers(a1, a2, day=23)  # 318020 9227674
