7e43045c893254463fe39096f7bc07f3f5c2066e
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-22 22:30:36 +0200

    2016 stuff

--- 2016/day_23.py
+++ 2016/day_23.py
@@ -1,12 +1,16 @@
 from collections import defaultdict
 from utils import read, print_answers
 
+
 def run(program, a):
+    program = program.copy()
     mem = defaultdict(int)
     mem["a"] = a
-    while mem["i"] < len(program):
+    j = 0
+    tgl = 0
+    while j < 100000 and mem["i"] < len(program):
+        j += 1
         cmd, *args = program[mem["i"]].split(" ")
-        # print(cmd, args, mem)
         match cmd:
             case "cpy":
                 exec(f"{args[1]} = {args[0]}; i += 1", {}, mem)
@@ -15,11 +19,19 @@ def run(program, a):
             case "dec":
                 exec(f"{args[0]} -= 1; i += 1", {}, mem)
             case "jnz":
-                exec(f"i += 1 if {args[0]} == 0 else {args[1]}",{},  mem)
+                # hack
+                if mem["i"] == 7:
+                    mem["a"] += mem["c"]
+                    mem["c"] = 0
+                if mem["i"] == 9:
+                    mem["a"] += mem["a"] * mem["d"]
+                    mem["d"] = 0
+                # /hack
+                exec(f"i += 1 if {args[0]} == 0 else {args[1]}", {}, mem)
             case "tgl":
                 ti = mem["i"] + eval(args[0], {}, mem)
-                # break
                 if 0 <= ti < len(program):
+                    tgl += 1
                     tc, *targs = program[ti].split(" ")
                     if len(targs) == 1:
                         ct = "dec" if tc == "inc" else "inc"
@@ -27,16 +39,13 @@ def run(program, a):
                     else:
                         ct = "cpy" if tc == "jnz" else "jnz"
                         program[ti] = f"{ct} {targs[0]} {targs[1]}"
-                    # break
                 mem["i"] += 1
     return mem["a"]
 
-program = read().split("\n")
-# program = read(2016, 23).split("\n")
-# run(program, 7)
 
+program = read(2016, 23).split("\n")
 
-# a1 = run(program)
-# a2 = run(program, part2=True)
+a1 = run(program, 7)
+a2 = run(program, 12)
 
-# print_answers(a1, a2, day=23)  # 318020 9227674
+print_answers(a1, a2, day=23)
