472d593eb7646614a267fc216b15733e09ff9e9c
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-22 11:32:13 +0200

    shorter

--- 2024/day_22.py
+++ 2024/day_22.py
@@ -1,23 +1,22 @@
 from utils import read, print_answers
 
-SIZE = 2000
-M = 16777216
 
 def step(n):
-    n = ((n*64) ^ n ) % M
-    n = ((n//32) ^ n ) % M
-    return ((n * 2048) ^ n) % M
+    n = ((n * 64) ^ n) % 16777216
+    n = ((n // 32) ^ n) % 16777216
+    return ((n * 2048) ^ n) % 16777216
+
 
 raw = read(2024, 22).split("\n")
 
 a1 = 0
 D = [{} for _ in range(len(raw))]
 for i, n in enumerate(map(int, raw)):
-    ns = [n] + [(n := step(n)) for _ in range(SIZE)]
-    ps = [x % 10 for x in ns]
-    ds = [b-a for a, b in zip(ps[:-1], ps[1:])]
-    chunks = [tuple(ds[i:i+4]) for i in range(len(ds)-3)]
+    ns = [n] + [(n := step(n)) for _ in range(2000)]
     a1 += ns[-1]
+    ps = [x % 10 for x in ns]
+    ds = [b - a for a, b in zip(ps[:-1], ps[1:])]
+    chunks = [tuple(ds[i : i + 4]) for i in range(len(ds) - 3)]
     for j, c in enumerate(chunks[::-1], 1):
         D[i][c] = ps[-j]
 
@@ -25,4 +24,3 @@ keys = set.union(*[set(d) for d in D])
 a2 = max(sum(d.get(k, 0) for d in D) for k in keys)
 
 print_answers(a1, a2, day=22)
-# 1931
