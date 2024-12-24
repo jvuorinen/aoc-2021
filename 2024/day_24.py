0cdcfa5931546df50739e1cdb119505717ae21b1
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-24 07:41:13 +0200

    24p1

--- 2024/day_24.py
+++ 2024/day_24.py
@@ -8,11 +8,34 @@ import networkx as nx
 from tqdm import tqdm
 from utils import read, print_answers
 
-raw = read().split("\n")
-# raw = read(2024, 24).split("\n")
+# a,b = read().split("\n\n")
+a, b = read(2024, 24).split("\n\n")
+start = a.split("\n")
+rules = b.split("\n")
 
+state = defaultdict()
+for x in start:
+    c, i = x.split(': ')
+    state[c] = int(i)
 
-a1 = None
+OPS = {
+    "AND": "&",
+    "OR": "|",
+    "XOR": "^"
+}
+
+while rules:
+    # print(rules)
+    x = rules.pop(0)
+    a, op, b, c = findall("(...) (AND|OR|XOR) (...) -> (...)", x)[0]
+    if a in state and b in state:
+        fun = f"{state[a]} {OPS[op]} {state[b]}"
+        print(x, fun)
+        state[c] = eval(fun)
+    else:
+        rules.append(x)
+
+a1 = int("".join(map(str, [state[x] for x in sorted(state)[::-1] if x.startswith("z")])), 2)
 a2 = None
 
 print_answers(a1, a2, day=24)
