cd15e4206f83919f7e76e9e16395f3a2040a75cd
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-25 07:16:03 +0200

    d25 messy

--- 2024/day_25.py
+++ 2024/day_25.py
@@ -8,11 +8,27 @@ import networkx as nx
 from tqdm import tqdm
 from utils import read, print_answers
 
-raw = read().split("\n")
-# raw = read(2024, 25).split("\n")
+# raw = read().split("\n\n")
+raw = read(2024, 25).split("\n\n")
 
+keys = []
+locks = []
+for r in raw:
+    arr = np.array([list(x) for x in r.split("\n")])
+    arr = np.rot90(arr)
+    if arr[0,0] == "#":
+        keys.append((arr == "#").astype(int))
+    else:
+        locks.append((arr == "#").astype(int))
 
-a1 = None
+a1 = 0
+for k, l in product(keys, locks):
+    s = k.shape
+    # print(k+l)
+    # print(((k+l) > 1).sum(), (k,l))
+    a1 += ((k+l) > 1).sum() == 0
+
+# a1 = None
 a2 = None
 
 print_answers(a1, a2, day=25)
