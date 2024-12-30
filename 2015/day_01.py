da53bb6852a57eb093b69e9c867278bc4a59d917
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2024-12-30 10:46:45 +0200

    cleaner

--- 2015/day_01.py
+++ 2015/day_01.py
@@ -1,7 +1,5 @@
 from utils import read, print_answers
 
-raw = read(2015, 1)
-
-diff = [(-1, 1)[c == "("] for c in raw]
+diff = [(-1, 1)[c == "("] for c in read(2015, 1)]
 pos = [p := 0] + [p := p + d for d in diff]
 print_answers(pos[-1], pos.index(-1), day=1)
---
+++ 2015/day_06.py
@@ -0,0 +1,17 @@
+from itertools import combinations, permutations, count, cycle
+from functools import reduce, cache
+from collections import Counter, defaultdict, deque
+from math import prod
+import numpy as np
+import re
+import networkx as nx
+from utils import read, print_answers
+
+raw = read().split("\n")
+# raw = read(2015, 6).split("\n")
+
+
+a1 = None
+a2 = None
+
+print_answers(a1, a2, day=6)
