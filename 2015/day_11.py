ecbaacb54c5ada7e14506628f314262b71278bfc
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2025-01-06 17:32:07 +0200

    d11

--- 2015/day_11.py
+++ 2015/day_11.py
@@ -4,33 +4,35 @@ from string import ascii_lowercase as ABC
 
 BASE = len(ABC)
 
-def baserep(n, base):
+
+def n_to_baserep(n, base):
     while n:
         n, m = divmod(n, base)
         yield m
 
-def to_int(br, base):
+
+def baserep_to_n(br, base):
     return sum(x * base**i for i, x in enumerate(br))
 
+
 def check(n):
-    br = list(baserep(n, BASE))
-    if set(br) & set(map(ABC.index, "iol")):
+    br = list(n_to_baserep(n, BASE))
+    if (1, 2) not in ((a - b, a - c) for a, b, c in zip(br[:-2], br[1:-1], br[2:])):
         return False
-    if (1, 2) not in [(a-b, a-c) for a, b, c in zip(br[:-2], br[1:-1], br[2:])]:
+    if len({n for n, g in groupby(br) if len(list(g)) >= 2}) <= 1:
         return False
-    return len({n for n, g in groupby(br) if len(list(g)) >= 2}) > 1
+    return not set(br) & set(map(ABC.index, "iol"))
 
 
-def rotate(pw):
+def rotator(pw):
     br = [ord(x) - ord("a") for x in pw[::-1]]
-    n = to_int(br, BASE)
-
-    for i in range(1, int(1E12)):
-        if check(n+i):
-            yield "".join(ABC[x] for x in baserep(n+i, BASE))[::-1]
+    n = baserep_to_n(br, BASE)
+    for i in range(1, int(1e12)):
+        if check(n + i):
+            yield "".join(ABC[x] for x in n_to_baserep(n + i, BASE))[::-1]
 
 
 pw = read(2015, 11)
-gen = rotate(pw)
+gen = rotator(pw)
 
 print_answers(next(gen), next(gen), day=11)
