4cbc55a65f03d13bf74eedc5060b086e4f1d91d8
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2025-01-16 09:12:41 +0200

    cleanear

--- 2015/day_25.py
+++ 2015/day_25.py
@@ -1,13 +1,15 @@
 import re
 from utils import read, print_answers
 
+
 def triang(n):
-    return n*(n+1) // 2
+    return n * (n + 1) // 2
+
 
 y, x = map(int, re.findall(r"\d+", read(2015, 25)))
 
-n = triang(x) + x * (y-1) + triang(y-2)
+n = triang(x) + x * (y - 1) + triang(y - 2)
 b, x, m = 20151125, 252533, 33554393
 
-a1 = b * pow(x, n-1, m) % m
+a1 = b * pow(x, n - 1, m) % m
 print_answers(a1, None, day=25)
