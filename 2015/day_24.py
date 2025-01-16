a498ff20ca11977c58fb9eddebc5239e19ebd91b
Jesse Vuorinen <jesse.vuorinen@sok.fi~>
2025-01-16 09:10:52 +0200

    done

--- 2015/day_24.py
+++ 2015/day_24.py
@@ -20,7 +20,7 @@ def evaluate(g, size, ps=None):
     if not ps:
         return True
     for _g in knapsack(sorted(ps)[::-1], size):
-        if evaluate(_g, size, ps - set(_g)):
+        if _g and evaluate(_g, size, ps - set(_g)):
             return prod(g)
 
 
@@ -32,3 +32,30 @@ def solve(n_groups):
 
 
 print_answers(solve(3), solve(4), day=24)
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def first_n_primes(n):
+    primes = []
+    num = 2
+    while len(primes) < n:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+# Get the first 40 primes
+first_40_primes = first_n_primes(40)
+print(first_40_primes)
